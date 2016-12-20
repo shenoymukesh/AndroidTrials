a3462be559a1077a99ba974e3a2ff3b26710bbfd
Mukesh Shenoy <shenoymukesh372@gmail.com>
2016-12-19 23:02:53 -0500

    Added UploadActivity.

---
+++ app/src/main/java/com/app/mukesh/androidtrials/UploadActivity.java
@@ -0,0 +1,16 @@
+package com.app.mukesh.androidtrials;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sahana on 12/19/16.
+ */
+
+public class UploadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
